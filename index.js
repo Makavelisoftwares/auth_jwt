295e99a5ed1cf5e6d2896ac1662c8126bf03c524
kavelicodes <makavelisoftwares@gmail.com>
2022-10-26 03:52:15 +0300

    assigned and used the third packages

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const cookieParser=require('cookie-parser');
 const bodyParser=require('body-parser');
 const morgan=require('morgan');
 const helmet=require('helmet');
-const {sequelize}=require('./models')
+const {sequelize}=require('./models');
 
 
+// using the middlewares and setting the view engine  
+app.use(cookieParser());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(helmet());
+app.use(morgan('combined'));
+app.use(express.static('public'));
+app.set('view engine','ejs');
